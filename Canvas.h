82253072bb08fbc1f9bdd785086e511a15f1d98a
Michel Lominy <michel-1.lominy@polymtl.ca>
2022-10-19 14:41:41 -0400

    ML: terminee l'implementation de l'algo de Bresenham, mais ne fonctionne pas bien

--- Canvas.h
+++ Canvas.h
@@ -24,12 +24,17 @@ public:
 	void drawBetween(int x1, int y1, int x2, int y2) {
 		int dx = abs(x2 - x1);
 		int dy = abs(y2 - y1);
-		if (dx > dy) 
-			plotPixel(x1, y1, x2, y2, dx, dy, 0);
-		else 
-			plotPixel(y1, x1, y2, x2, dy, dx, 1);
-        int firstDataIndex = getIndexOfWindowPos(x1, y1);
-        glBufferSubData(GL_ARRAY_BUFFER, firstDataIndex * sizeof(Point), dx * sizeof(Point), &points[firstDataIndex]);
+        int firstPixelIndex = getIndexOfWindowPos(x1, y1);
+
+        if (dx > dy) {
+            plotPixel(x1, y1, x2, y2, dx, dy, 0);
+            glBufferSubData(GL_ARRAY_BUFFER, firstPixelIndex * sizeof(Point), dx * sizeof(Point), &points[firstPixelIndex]);
+        }
+        else {
+            plotPixel(y1, x1, y2, x2, dy, dx, 1);
+            glBufferSubData(GL_ARRAY_BUFFER, firstPixelIndex * sizeof(Point), dy * sizeof(Point), &points[firstPixelIndex]);
+        }
+
 	}
 	
     void plotPixel(int x1, int y1, int x2, int y2, int dx, int dy, int decide)
@@ -41,13 +46,12 @@ public:
                 if (decide == 0) {
                     points[getIndexOfWindowPos(x1, y1)].color[0] = 1.0;
                     // putpixel(x1, y1, RED);
-                    pk = pk + 2 * dy;
                 }
                 else {
-                    points[getIndexOfWindowPos(y1, x1)].color[0] = 1.0;
+                    points[getIndexOfWindowPos(y1, x1)].color[1] = 1.0;
                     // putpixel(y1, x1, YELLOW);
-                    pk = pk + 2 * dy;
                 }
+                pk = pk + 2 * dy;
             }
             else {
                 y1 < y2 ? y1++ : y1--;
@@ -56,7 +60,7 @@ public:
                     // putpixel(x1, y1, RED);
                 }
                 else {
-                    points[getIndexOfWindowPos(y1, x1)].color[0] = 1.0;
+                    points[getIndexOfWindowPos(y1, x1)].color[1] = 1.0;
                     //  putpixel(y1, x1, YELLOW);
                 }
                 pk = pk + 2 * dy - 2 * dx;
